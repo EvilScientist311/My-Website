da144ecb3bdedd25630253ca305c29e6ea4a7ae7
EvilScientist311 <114818194+EvilScientist311@users.noreply.github.com>
2026-05-24 17:38:35 +1000

    mobile site fixes

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -1,44 +1,95 @@
 /*
-* Greedy Navigation
-*
-* http://codepen.io/lukejacksonn/pen/PwmwWV
-*
-*/
+ * Greedy Navigation
+ *
+ * http://codepen.io/lukejacksonn/pen/PwmwWV
+ *
+ */
 
-var $nav = $('#site-nav');
-var $btn = $('#site-nav button');
-var $vlinks = $('#site-nav .visible-links');
+var $nav = $("#site-nav");
+var $btn = $("#site-nav button");
+var $vlinks = $("#site-nav .visible-links");
 var $vlinks_persist_tail = $vlinks.children("*.persist.tail");
-var $hlinks = $('#site-nav .hidden-links');
+var $hlinks = $("#site-nav .hidden-links");
 
 var breaks = [];
+var MOBILE_BREAKPOINT = 768;
+
+function isMobileNav() {
+  return window.innerWidth <= MOBILE_BREAKPOINT;
+}
+
+function updateMastheadPadding() {
+  var mastheadHeight = $(".masthead").height();
+  $("body").css("padding-top", mastheadHeight + "px");
+
+  if ($(".author__urls-wrapper button").is(":visible")) {
+    $(".sidebar").css("padding-top", "");
+  } else {
+    $(".sidebar").css("padding-top", mastheadHeight + "px");
+  }
+}
+
+function closeHiddenLinks() {
+  $hlinks.addClass("hidden");
+  $btn.removeClass("close");
+  $btn.attr("aria-expanded", "false");
+}
+
+function openHiddenLinks() {
+  $hlinks.removeClass("hidden");
+  $btn.addClass("close");
+  $btn.attr("aria-expanded", "true");
+}
+
+function restoreAllLinksToVisible() {
+  while ($hlinks.children().length > 0) {
+    if ($vlinks_persist_tail.children().length > 0) {
+      $hlinks.children().first().insertBefore($vlinks_persist_tail);
+    } else {
+      $hlinks.children().first().appendTo($vlinks);
+    }
+  }
+
+  breaks = [];
+}
+
+function collapseAllLinksForMobile() {
+  while ($vlinks.children("li:not(.persist)").length > 0) {
+    $vlinks.children("li:not(.persist)").last().prependTo($hlinks);
+  }
+
+  breaks = [];
+  $btn.removeClass("hidden");
+}
 
 function updateNav() {
+  updateMastheadPadding();
 
-  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
+  if (isMobileNav()) {
+    collapseAllLinksForMobile();
+    return;
+  }
 
-  // The visible list is overflowing the nav
+  restoreAllLinksToVisible();
+
+  var availableSpace = $btn.hasClass("hidden")
+    ? $nav.width()
+    : $nav.width() - $btn.width() - 30;
+
   if ($vlinks.width() > availableSpace) {
-
-    while ($vlinks.width() > availableSpace && $vlinks.children("*:not(.persist)").length > 0) {
-      // Record the width of the list
+    while (
+      $vlinks.width() > availableSpace &&
+      $vlinks.children("*:not(.persist)").length > 0
+    ) {
       breaks.push($vlinks.width());
-
-      // Move item to the hidden list
       $vlinks.children("*:not(.persist)").last().prependTo($hlinks);
-
-      availableSpace = $btn.hasClass("hidden") ? $nav.width() : $nav.width() - $btn.width() - 30;
-
-      // Show the dropdown btn
+      availableSpace = $btn.hasClass("hidden")
+        ? $nav.width()
+        : $nav.width() - $btn.width() - 30;
       $btn.removeClass("hidden");
     }
-
-    // The visible list is not overflowing
   } else {
-
-    // There is space for another item in the nav
     while (breaks.length > 0 && availableSpace > breaks[breaks.length - 1]) {
-      // Move the item to the visible list
       if ($vlinks_persist_tail.children().length > 0) {
         $hlinks.children().first().insertBefore($vlinks_persist_tail);
       } else {
@@ -47,40 +98,67 @@ function updateNav() {
       breaks.pop();
     }
 
-    // Hide the dropdown btn if hidden list is empty
     if (breaks.length < 1) {
-      $btn.addClass('hidden');
-      $btn.removeClass('close');
-      $hlinks.addClass('hidden');
+      $btn.addClass("hidden");
+      $btn.removeClass("close");
+      $hlinks.addClass("hidden");
+      $btn.attr("aria-expanded", "false");
     }
   }
 
-  // Keep counter updated
   $btn.attr("count", breaks.length);
-
-  // update masthead height and the body/sidebar top padding
-  var mastheadHeight = $('.masthead').height();
-  $('body').css('padding-top', mastheadHeight + 'px');
-  if ($(".author__urls-wrapper button").is(":visible")) {
-    $(".sidebar").css("padding-top", "");
-  } else {
-    $(".sidebar").css("padding-top", mastheadHeight + "px");
-  }
-
 }
 
-// Window listeners
-
-$(window).on('resize', function () {
-  updateNav();
-});
-screen.orientation.addEventListener("change", function () {
-  updateNav();
+$btn.attr({
+  type: "button",
+  "aria-label": "Menu",
+  "aria-expanded": "false",
+  "aria-controls": "site-nav-menu",
 });
 
-$btn.on('click', function () {
-  $hlinks.toggleClass('hidden');
-  $(this).toggleClass('close');
+$hlinks.attr("id", "site-nav-menu");
+
+$btn.on("click", function (event) {
+  event.stopPropagation();
+
+  if ($hlinks.hasClass("hidden")) {
+    openHiddenLinks();
+  } else {
+    closeHiddenLinks();
+  }
 });
+
+$(document).on("click.mobileNav", function (event) {
+  if (
+    isMobileNav() &&
+    !$hlinks.hasClass("hidden") &&
+    !$nav.is(event.target) &&
+    $nav.has(event.target).length === 0
+  ) {
+    closeHiddenLinks();
+  }
+});
+
+$(document).on("keydown.mobileNav", function (event) {
+  if (event.key === "Escape") {
+    closeHiddenLinks();
+  }
+});
+
+$hlinks.on("click", "a", function () {
+  if (isMobileNav()) {
+    closeHiddenLinks();
+  }
+});
+
+$(window).on("resize", function () {
+  updateNav();
+});
+
+if (screen.orientation) {
+  screen.orientation.addEventListener("change", function () {
+    updateNav();
+  });
+}
 
 updateNav();
